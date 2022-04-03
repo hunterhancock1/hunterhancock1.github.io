02f15c3b0aa6a4a335fa54f66ef7a5511beb14d2
hunterhancock1 <hunter.hancock@colorado.edu>
2022-04-03 14:46:07 -0600

    img

--- js/11.js
+++ js/11.js
@@ -3,3 +3,9 @@ document.getElementById("e1").addEventListener("click", clickButton);
 function clickButton() {
     document.getElementById("e1p").innerHTML = "You clicked the button! Nice!";
 }
+
+document.getElementById("e2").addEventListener("mouseover", changeImage);
+
+function changeImage() {
+    document.getElementById("e2").src = "../img/ws2.png";
+}
