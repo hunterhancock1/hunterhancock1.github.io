757d1c04bd3114866d8d378d7c7cdd21a812167b
hunterhancock1 <hunter.hancock@colorado.edu>
2022-04-03 15:12:42 -0600

    js

--- js/11.js
+++ js/11.js
@@ -4,7 +4,7 @@ function clickButton() {
     document.getElementById("e1p").innerHTML = "You clicked the button! Nice!";
 }
 
-document.getElementById("e2").addEventListener("mouseover", hoverImage);
+document.getElementById("e2").addEventListener("click", hoverImage);
 
 function hoverImage() {
     document.getElementById("ep2").innerHTML = "Keep my wife's name out your f*cking mouth";
