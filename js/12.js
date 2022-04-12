ebcf60e682cdf9e4870824fc93284658b9cdf0a2
hunterhancock1 <hunter.hancock@colorado.edu>
2022-04-12 00:50:12 -0600

    revert

--- js/12.js
+++ js/12.js
@@ -1,6 +1,22 @@
-const quoteButton = document.querySelector('#newQuote');
-quoteButton.addEventListener('click', getQuote);
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("newQuote");
+    const quote = document.getElementById("quote");
+    const author = document.getElementById("author");
+  
+    async function updateQuote() {
+      const response = await fetch("https://api.quotable.io/random");
+      const data = await response.json();
 
-function getQuote() {
-    console.log("button pressed");
-}
+    if (response.ok) {
+        quote.textContent = data.content;
+        author.textContent = data.author;
+    } else {
+        quote.textContent = "An error occured";
+        console.log(data);
+    }
+    }
+
+    button.addEventListener("click", updateQuote);
+    updateQuote();
+});
+  
