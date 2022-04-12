27061232583ee92f626383b934c8af04484b01f4
hunterhancock1 <hunter.hancock@colorado.edu>
2022-04-12 00:21:18 -0600

    Update 12.js

--- js/12.js
+++ js/12.js
@@ -5,6 +5,8 @@ async function newQuote() {
     fetch("https://api.quotable.io/random");
     const quote = response.json()
 
+    console.log(quote);
+
     document.getElementById("quote").innerHTML = quote.content;
     document.getElementById("author").innerHTML = quote.author;
 }
