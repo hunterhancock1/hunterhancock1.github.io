a8f447bfa5eba1c9b559879eb7335c76c52faaec
hunterhancock1 <hunter.hancock@colorado.edu>
2022-04-12 00:33:33 -0600

    Summary (required)

--- js/12.js
+++ js/12.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
       // Fetch a random quote from the Quotable API
       const response = await fetch("https://api.quotable.io/random");
       const data = await response.json();
-      
+
       if (response.ok) {
         // Update DOM elements
         quote.textContent = data.content;
@@ -21,5 +21,5 @@ document.addEventListener("DOMContentLoaded", () => {
     // Attach an event listener to the `button`
     button.addEventListener("click", updateQuote);
     updateQuote();
-  });
+});
   
